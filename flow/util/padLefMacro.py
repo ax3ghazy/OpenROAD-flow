16ebae0061b5406933f74adfc8678f2d6254af97
ax3ghazy <ax3ghazy@aucegypt.edu>
2020-02-04 20:58:41 +0200

    Update padLefMacro.py

--- flow/util/padLefMacro.py
+++ flow/util/padLefMacro.py
@@ -8,11 +8,20 @@ import argparse  # argument parsing
 # ==============================================================================
 parser = argparse.ArgumentParser(
     description='Adds padding to the right of all macros in a lef file')
-parser.add_argument('--right', '-r', required=True, type=int,
+parser.add_argument('--right', '-r', required=False, type=int,
+                    default='0',
                     help='Padding on the right in SITE widths')
-parser.add_argument('--left', '-l', required=True, type=int,
+parser.add_argument('--left', '-l', required=False, type=int,
+                    default='0',
                     help='Padding on the left in SITE widths')
-parser.add_argument('--site', '-s', required=True,
+parser.add_argument('--top', '-t', required=False, type=int,
+                    default='0',
+                    help='Padding on the top in SITE heights')
+parser.add_argument('--bottom', '-b', required=False, type=int,
+                    default='0',
+                    help='Padding on the bottom in SITE heights')
+parser.add_argument('--site', '-s', required=False,
+                    default='0',
                     help='Lef SITE')
 parser.add_argument('--exclude', '-e', required=False,
                     default='ENDCAPTIE* CNRCAP* INCNR* TBCAP* FILL* WELLTAP* tsmc65lp_* gf14_* fakeram45_*',
@@ -23,22 +32,24 @@ parser.add_argument('--outputLef', '-o', required=True,
                     help='Output LEF')
 args = parser.parse_args()
 
+REGEXNUM = "-?\d+\.?\d*"
 
 def replace_size(match):
   m = match.groups()
   newWidth = float(m[0]) + right_padding + left_padding
-  return "SIZE " + '{0:g}'.format(newWidth) + " BY " + m[1]
+  newHeight = float(m[1]) + top_padding + bottom_padding
+  return "SIZE " + '{0:g}'.format(newWidth) + " BY " + '{0:g}'.format(newHeight)
 
 def replace_rect(match):
   m = match.groups()
-  pt1 = '{0:g}'.format(float(m[0]) + left_padding) + " " + m[1]
-  pt2 = '{0:g}'.format(float(m[2]) + left_padding) + " " + m[3]
+  pt1 = '{0:g}'.format(float(m[0]) + left_padding) + " " + '{0:g}'.format(float(m[1]) + bottom_padding)
+  pt2 = '{0:g}'.format(float(m[2]) + left_padding) + " " + '{0:g}'.format(float(m[3]) + bottom_padding)
   return "RECT " + pt1 + " " + pt2
 
 def replace_rectMask(match):
   m = match.groups()
-  pt1 = '{0:g}'.format(float(m[1]) + left_padding) + " " + m[2]
-  pt2 = '{0:g}'.format(float(m[3]) + left_padding) + " " + m[4]
+  pt1 = '{0:g}'.format(float(m[1]) + left_padding) + " " + '{0:g}'.format(float(m[2]) + bottom_padding)
+  pt2 = '{0:g}'.format(float(m[3]) + left_padding) + " " + '{0:g}'.format(float(m[4]) + bottom_padding)
   return "RECT MASK " + str(m[0]) + " " + pt1 + " " + pt2
 
 # Function used by re.sub
@@ -55,15 +66,15 @@ def replace_pad(match):
   returnString = match.group()
 
   # Pad SIZE
-  sizePattern = r"SIZE (\S+) BY (\S+)"
+  sizePattern = f"^\s*SIZE\s+({REGEXNUM})\s+BY\s+({REGEXNUM})"
   returnString = re.sub(sizePattern, replace_size, returnString, 0, re.M | re.DOTALL)
 
   # Pad RECTs
-  rectPattern = r"RECT (\S+) (\S+) (\S+) (\S+)\n"
+  rectPattern = f"^\s*RECT\s+({REGEXNUM})\s+({REGEXNUM})\s+({REGEXNUM})\s+({REGEXNUM})"
   returnString = re.sub(rectPattern, replace_rect, returnString, 0, re.M | re.DOTALL)
 
   # Pad RECT MASKs
-  rectMastPattern = r"RECT MASK (\S+) (\S+) (\S+) (\S+) (\S+)\n"
+  rectMastPattern = f"^\s*RECT\s+MASK\s+({REGEXNUM})\s+({REGEXNUM})\s+({REGEXNUM})\s+({REGEXNUM})"
   returnString = re.sub(rectMastPattern, replace_rectMask, returnString, 0, re.M | re.DOTALL)
 
   return returnString
@@ -77,26 +88,30 @@ content = f.read()
 f.close()
 
 # Find SITE width
-sitePattern = r"SITE\s" + args.site + ".*?SIZE\s(\S+)\sBY\s(\S+)"
+sitePattern = f"^SITE\s+{args.site}.*SIZE\s+({REGEXNUM})\s+BY\s+({REGEXNUM}).*END\s+{args.site}"
 m = re.search(sitePattern, content, re.M | re.DOTALL)
 
 if m:
   site_width = float(m.group(1))
+  site_height = float(m.group(2))
   right_padding = float(args.right) * site_width
   left_padding = float(args.left) * site_width
+  top_padding = float(args.top) * site_height
+  bottom_padding = float(args.bottom) * site_height
 else:
   raise ValueError("Error: Pattern search for SITE size failed")
 
 print("Derived SITE width (microns): " + str(site_width))
+print("Derived SITE height (microns): " + str(site_height))
 print("Right cell padding (microns): " + str(right_padding))
 print("Left cell padding (microns): " + str(left_padding))
+print("Top cell padding (microns): " + str(top_padding))
+print("Bottom cell padding (microns): " + str(bottom_padding))
 
 
 # Perform substitution on every macro
 pattern = r"^MACRO\s+(\S+).*?^END\s\S+"
 result, count = re.subn(pattern, replace_pad, content, 0, re.M | re.DOTALL)
-
-
 
 # Write output file
 f = open(args.outputLef, "w")
